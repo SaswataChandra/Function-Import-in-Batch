68ae0884195d62ea59266a1063570011a92dce49
SaswataChandra <61310910+SaswataChandra@users.noreply.github.com>
2022-08-03 10:29:58 +0530

    Create Component.js

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.deere.digisignature.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
